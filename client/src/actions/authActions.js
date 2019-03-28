2af739a79221eaab70510b9039d90e1a0f404c32
hnekhniadovich <nekhanya@gmail.com>
2019-03-28 13:14:33 -0400

    authentication

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
-import { GET_ERRORS } from './types';
+import setAuthToken from '../utils/setAuthToken';
+import jwt_decode from 'jwt-decode';
+
+import { GET_ERRORS, SET_CURRENT_USER } from './types';
 
 // Register User
 export const registerUser = (userData, history) => dispatch => {
@@ -15,3 +18,42 @@ export const registerUser = (userData, history) => dispatch => {
 };
 
 // Login - Get User Token
+export const loginUser = (userData) => dispatch => {
+    axios
+        .post('/api/users/login', userData)
+        .then(res => {
+            // Save to local storage
+            const { token } = res.data;
+            // Set token to ls
+            localStorage.setItem('jwtToken', token);
+            // Set token to Auth header
+            setAuthToken(token);
+            // Decode token to get user data
+            const decoded = jwt_decode(token);
+            // Set current user
+            dispatch(setCurrentUser(decoded));
+        })
+        .catch(err =>
+            dispatch({
+                type: GET_ERRORS, 
+                payload: err.response.data
+            })
+        );
+};
+
+// Set logged in user
+export const setCurrentUser = (decoded) => {
+    return {
+        type: SET_CURRENT_USER,
+        payload: decoded
+    };   
+};
+
+// Log user out
+export const logoutUser = () => dispatch => {
+    localStorage.removeItem('jwtToken');
+    // Remove auth header for future requests
+    setAuthToken(false);
+    // Set current user to {} which will set isAuthenticated to false
+    dispatch(setCurrentUser({}));
+}
--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,19 +1,37 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import classnames from 'classnames';
+import { loginUser } from '../../actions/authActions';
+import TextFieldGroup from '../common/TextFieldGroup';
 
 class Login extends Component {
 
     constructor() {
         super();
         this.state = {
-            name: '',
             email: '',
             password: '',
-            password2: '',
             errors: {}
         }
 
         this.onChange = this.onChange.bind(this);
         this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    componentDidMount() {
+        if(this.props.auth.isAuthenticated) {
+            this.props.history.push('/dashboard');
+        }
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if(nextProps.auth.isAuthenticated) {
+            this.props.history.push('/dashboard');
+        }
+        if(nextProps.errors) {
+            this.setState({errors: nextProps.errors})
+        }    
     }
 
     onChange(e)  {
@@ -23,15 +41,17 @@ class Login extends Component {
     onSubmit(e) {
         e.preventDefault();
 
-        const newUser = {
-            name: this.state.email,
+        const userData = {
+            email: this.state.email,
             password: this.state.password
         }
 
-        console.log(newUser);
+        this.props.loginUser(userData)
     }
 
     render() {
+        const { errors } = this.state;
+
         return (
             <div className="login">
                 <div className="container">
@@ -39,25 +59,25 @@ class Login extends Component {
                         <div className="col-md-8 m-auto">
                         <h1 className="display-4 text-center">Log In</h1>
                         <p className="lead text-center">Sign in to your DevConnector account</p>
-                        <form onSubmit={this.onSubmit}>
-                            <div className="form-group">
-                            <input 
-                                type="email" 
-                                className="form-control form-control-lg" 
-                                placeholder="Email Address" 
+                        <form noValidate onSubmit={this.onSubmit}>
+                            <TextFieldGroup
+                                placeholder="Email Address"
                                 name="email"
+                                type="email"
                                 value={this.state.email}
-                                onChange={this.onChange} />
-                            </div>
-                            <div className="form-group">
-                            <input 
-                                type="password" 
-                                className="form-control form-control-lg" 
-                                placeholder="Password" 
+                                onChange={this.onChange}
+                                error={errors.email}
+                            />
+
+                            <TextFieldGroup
+                                placeholder="Password"
                                 name="password"
+                                type="password"
                                 value={this.state.password}
-                                onChange={this.onChange} />
-                            </div>
+                                onChange={this.onChange}
+                                error={errors.password}
+                            />
+                            
                             <input 
                                 type="submit" 
                                 className="btn btn-info btn-block mt-4" />
@@ -71,4 +91,15 @@ class Login extends Component {
   }
 }
 
-export default Login;
+Login.propTypes = {
+    loginUser: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired,
+    errors: PropTypes.object.isRequired
+}
+
+const mapStateToProps = (state) => ({
+    auth: state.auth,
+    errors: state.errors
+})
+
+export default connect(mapStateToProps, { loginUser })(Login);
