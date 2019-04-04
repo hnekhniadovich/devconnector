50668ba99dbe48a14f929604fe3deb9159b66518
hnekhniadovich <nekhanya@gmail.com>
2019-04-04 12:22:06 -0400

    profiles dashboard

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-import { GET_PROFILE, 
+import { GET_PROFILE,
+         GET_PROFILES, 
          PROFILE_LOADING, 
          CLEAR_CURRENT_PROFILE,
          GET_ERRORS,
@@ -20,6 +21,22 @@ export const getCurrentProfile = () => dispatch => {
                 dispatch({
                     type: GET_PROFILE,
                     payload: {}
+                }))
+}
+
+// Get profile by handle
+export const getProfileByHandle = (handle) => dispatch => {
+    dispatch(setProfileLoading());
+    axios.get(`/api/profile/handle/${handle}`)
+        .then(res => 
+            dispatch({
+                type: GET_PROFILE,
+                payload: res.data
+            }))
+            .catch(err => 
+                dispatch({
+                    type: GET_PROFILE,
+                    payload: null
                 }))
 }
 
@@ -92,6 +109,25 @@ export const deleteEducation = id => dispatch => {
             dispatch({
             type: GET_ERRORS,
             payload: err.response.data
+            })
+        );
+};
+
+// Get All Profiles
+export const getProfiles = () => dispatch => {
+    dispatch(setProfileLoading());
+    axios
+        .get('/api/profile/all')
+        .then(res =>
+            dispatch({
+            type: GET_PROFILES,
+            payload: res.data
+            })
+        )
+        .catch(err =>
+            dispatch({
+            type: GET_PROFILES,
+            payload: null
             })
         );
 };
