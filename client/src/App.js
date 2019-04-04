69ca9812f4307b7f93cdfd2cbc30798b80652838
hnekhniadovich <nekhanya@gmail.com>
2019-04-04 15:03:18 -0400

    profile display touch up

--- client/src/App.js
+++ client/src/App.js
@@ -22,9 +22,9 @@ import AddExperience from './components/add-credentials/AddExperience';
 import AddEducation from './components/add-credentials/AddEducation';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
+import NotFound from './components/not-found/NotFound';
 
 import './App.css';
-
 
 // Check for token
 if(localStorage.jwtToken) {
@@ -75,6 +75,7 @@ class App extends Component {
               <Switch>
                 <PrivateRoute exact path="/add-education" component={AddEducation} />
               </Switch>
+              <Route exact path="/not-found" component={NotFound} />
             </div>
             <Footer />
           </div>
